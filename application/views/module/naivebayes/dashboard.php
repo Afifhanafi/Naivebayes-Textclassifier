1c33476d0e4c15c631d74fcbf11a5e64548a05c0
Afifhanafi <hanafiafif23@gmail.com>
2024-12-17 05:27:33 +0700

    First commit

---
+++ application/views/module/naivebayes/dashboard.php
@@ -0,0 +1,7 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="card-box" align="center">
+          <h3>Nama Kalian :: Text Mining Metode Naive Bayes</h3>
+        </div>
+    </div>
+</div>
